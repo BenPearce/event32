ca9e8e6c4230986692f05b9b79f349f5c165d8b8
Your Name HereBen <benpearce1@gmail.com>
2013-11-29 01:18:19 -0800

    event expand progress

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -58,36 +58,12 @@ function populateCalendar(friendIdList){
 */
 
 function populateDate(k){
-	//alert("popdate: "+k);
-	var exHeight = (50*k) + $("#event-list-wrap-"+k).height();
-	//alert(exHeight);
-	//$("#event-list-wrap-" + i).animate('{height:exHeight'+"px"+'}');
-        //$("#event-list-wrap-trans-"+ i).animate({height:"3000px"});
-        //$("#event-list-wrap-trans-"+ i).css("{height:2000px;transition: height 2s;-webkit-transition: width 2s;}");
-       // $("#event-list-wrap-" + i).css("{border:solid green 3px;}");
-          //$("#event-list-wrap-"+i).css("{height:2000px;}");
-           //$("#event-list-wrap-"+i).css('height','2000px');
-            //$("#event-list-wrap-"+k).animate({height:"1000px"}, 500);
-            //var animateInput = '{height:"'+exHeight+'px"}';
-            // var animateInput = '{height:"'+exHeight+'px"}, 500';
-            //{height:"150px"}
-            //alert(animateInput);
-            //$("#event-list-wrap-"+k).animate(animateInput);
+	var exHeight = (74*k) + $("#event-list-wrap-"+k).height();
             $("#event-list-wrap-"+k).animate({height:exHeight}, 500);
-            //$("#event-list-wrap-"+k).animate({height:"px"+exHeight}, 500);
             
-            //$("#event-list-wrap-"+k).css("{height:2000px;transition: height 2s;-webkit-transition: height 2s;}");
-            //alert($("#event-list-wrap-"+k).html());
-        //{
-        
-        	/*
-{height:2000px;transition: height 2s;
--webkit-transition: width 2s;}
-}
-*/
 	for (var i = 0; i < eveningHash[k].eventIdArray.length; i++) {
-		console.log(eveningHash[k].eventIdArray[i]);
-		//$("#event-list-wrap-" + i).append(getEventRow(fbArray[eveningHash[k].eventIdArray[i]].fbId));
+		//console.log(eveningHash[k].eventIdArray[i]);
+		$("#event-list-wrap-" +k).append(getEventRow(fbArray[eveningHash[k].eventIdArray[i]].fbId));
 	}
 }
 
