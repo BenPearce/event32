d8971ce584288e77b9698e1df35aa7baf20a35c4
Your Name HereBen <benpearce1@gmail.com>
2013-12-01 19:12:42 -0800

    formatting enhancements

--- platforms/ios/www/js/imageCluster.js
+++ platforms/ios/www/js/imageCluster.js
@@ -25,7 +25,7 @@
           	 
             	 if(fbArray[eventId].friendIdArray.length == 0){
             	 
-            	 return '<div class="row-wrap"><div class="event-wrap '+eventClass+'"><div class="row-pic event-pic">'+eventImg+'</div><div class="row-title-wrap event-title-wrap"><div class="row-title event-title">'+eventName+'</div></div></div><div class="friend-wrap"><div class="row-pic-wrap friend-pic-wrap" id="friend-'+eventId+'"><div class="row-pic friend-pic">'+friendImg+'</div></div><div class="row-title-wrap friend-title-wrap"><div class="row-title friend-title">'+friendName+'</div></div></div></div>';
+            	 return '<div class="row-wrap"><div class="event-wrap '+eventClass+'"><div class="row-pic event-pic">'+eventImg+'</div><div class="row-title-wrap event-title-wrap"><div class="row-title event-title">'+eventName+'</div></div></div><div class="friend-wrap"><div class="row-pic-wrap friend-pic-wrap" id="friend-'+eventId+'"><div class="row-pic friend-pic">'+friendImg+'</div></div></div></div>';
             	 
             	 } else if (fbArray[eventId].friendIdArray.length == 1){
             	 
@@ -37,7 +37,7 @@
             	 var pre = '<img class="event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="';      
 		 var preUL = '<img class="quadUl event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="'; 
 		 var preUR = '<img class="quadUr event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="'; 
-		  var preBL = '<img class="quadBl event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="'; 
+		 var preBL = '<img class="quadBl event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="'; 
 		 var preBR = '<img class="quadBr event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="'; 
 	         var post = '">'; 
 	         var friendImgQuad = typeof fbArray[eventId].friendIdArray[0] != "undefined"  ? friendPre + "https://graph.facebook.com/"+fbArray[fbArray[eventId].friendIdArray[0]].fbId +"/picture?width=25&height=25" + post : friendPre + "images/pinkX.png"+ post;
@@ -50,7 +50,7 @@
                  var three = typeof fbArray[eventId].friendIdArray[3] != "undefined" ? preBR + "https://graph.facebook.com/"+fbArray[fbArray[eventId].friendIdArray[3]].fbId +"/picture?width=25&height=25" + post : preBR + "images/pinkX.png"+ post;
 
                  //return '<div class="row-wrap"><div class="event-wrap '+eventClass+'"><div class="row-pic-wrap event-pic-wrap"><div class="row-pic event-pic">'+eventImg+'</div></div><div class="row-title-wrap event-title-wrap"><div class="row-title event-title">'+eventName+'</div></div></div><div class="friend-wrap" id="friend-'+eventId+'"><div class="attendee-quad-row"><div class="friend-pic-wrap-quad"><div class="friend-pic-quad">'+zero+'</div></div><div class="friend-pic-wrap-quad"><div class="friend-pic-quad">'+one+'</div></div></div><div class="attendee-quad-row"><div class="friend-pic-wrap-quad"><div class="friend-pic-quad">'+two+'</div></div><div class="friend-pic-wrap-quad"><div class="friend-pic-quad">'+three+'</div></div></div></div></div><div class="row-title-wrap friend-title-wrap"><div class="row-title friend-title">'+friendNameQuad+'</div></div></div>';
-                 return '<div class="row-wrap"><div class="event-wrap '+eventClass+'"><div class="row-pic-wrap event-pic-wrap"><div class="row-pic event-pic">'+eventImg+'</div></div><div class="row-title-wrap event-title-wrap">'+eventName+'</div></div><div class="friend-wrap" id="friend-'+eventId+'">'+zero+one+two+three+'</div></div><div class="row-title-wrap friend-title-wrap"><div class="row-title friend-title">'+friendNameQuad+'</div></div></div>';
+                 return '<div class="row-wrap"><div class="event-wrap '+eventClass+'"><div class="row-pic-wrap event-pic-wrap"><div class="row-pic event-pic">'+eventImg+'</div></div><div class="row-title-wrap event-title-wrap">'+eventName+'</div></div><div class="friend-wrap" id="friend-'+eventId+'">'+zero+one+two+three+'</div></div></div>';
 
             	 }
             }
