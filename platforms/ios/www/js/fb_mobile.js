f8bf4ac8ca18d26731cea3bf173dd6bf88c8ccd6
Your Name HereBen <benpearce1@gmail.com>
2013-12-14 20:11:10 -0800

    added $.Deferred to createTable()

--- platforms/ios/www/js/fb_mobile.js
+++ platforms/ios/www/js/fb_mobile.js
@@ -156,6 +156,27 @@ var updateEventAttr = function () {
     return dfd.promise();
 }
 
+
+function createTable1(){
+    var dfd = $.Deferred();
+    var db1 = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+    //db1.transaction(createTable, errorCB, function (tx) {
+    db1.transaction(function (tx) {
+    tx.executeSql('DROP TABLE IF EXISTS FRIENDS');
+    tx.executeSql('DROP TABLE IF EXISTS FRIENDS_EVENTS');
+    tx.executeSql('DROP TABLE IF EXISTS EVENTS');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS FRIENDS (fbId unique, name,touched DATETIME)');
+    tx.executeSql('CREATE TABLE FRIENDS_EVENTS(id unique,touched DATETIME DEFAULT CURRENT_TIMESTAMP,startTime DATETIME,eventFbId,friendFbId,UNIQUE(eventFbId, friendFbId))');
+
+    tx.executeSql('CREATE TABLE EVENTS(id unique,touched DATETIME DEFAULT CURRENT_TIMESTAMP,start_time DATETIME,update_time DATETIME,eventFbId unique, name, description, end_time,attending_count,pic,pic_big,pic_square,pic_cover,ticket_uri,timezone,unsure_count,venue_street,venue_city,venue_state,venue_country,venue_zip,venue_latitude,venue_longitude,venue_id,venue_name,venue_located_in,pic_small,all_members_count,can_invite_friends,creator,declined_count,app_id,feed_targeting,has_profile_pic,host,is_date_only,not_replied_count,privacy)');
+
+                    },errorCB,function(){
+                         dfd.resolve("friendIdList1");
+                    });
+    return dfd.promise();
+
+}
+
 document.addEventListener('deviceready', function () {
                           window.fbAsyncInit = function () {
                           init();
@@ -174,16 +195,23 @@ function init() {
                       if (response.status == "connected") {
                       alert("connected");
                       accessToken = response.authResponse.accessToken;
-                      var db1 = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-                      db1.transaction(createTable, errorCB, function (tx) {
-                                      updateFriends().done(
-                                                           function () {
+                      
+                      
+                      //var db1 = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+                      //db1.transaction(createTable, errorCB, function (tx) {
+                      createTable1().done(function(){
+                                          
+                                       
+                                      
+                                      updateFriends().done(function () {
                                                            updateEvents().done(function () {
                                                                                updateEventAttr();
                                                                                });
-                                                           }
-                                                           );
-                                      });
+                                                           });
+                                          });
+                                      //});
+                      
+                      
                       } else if (response.status == "not_authorized") {
                       alert("not_authorized");
                       $("#fb-login-button").text("Facebook Authorization");
