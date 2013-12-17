ffcabc6eff87445a08449ef48528a3ae9d03dd90
Your Name HereBen <benpearce1@gmail.com>
2013-12-16 19:37:20 -0800

    progress

--- platforms/ios/www/js/fb_mobile.js
+++ platforms/ios/www/js/fb_mobile.js
@@ -190,7 +190,13 @@ function insertEventArrtDb(eventAttrParse) {
                     //console.log("test: "+test);
                     tx.executeSql("INSERT INTO EVENTS ("+'formattedTime'+","
                                   +'formattedDateTime'+","
-                                  +'formattedDate'+","+'eventFbId','touched'+","+'start_time'+",'update_time','end_time','description','name','attending_count','unsure_count','not_replied_count','all_members_count','timezone','ticket_uri','pic_small','pic','pic_big','pic_square','pic_cover','can_invite_friends','creator') VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                                  +'formattedDate'+","
+                                  +'eventFbId'+","
+                                  +'touched'+","
+                                  +'start_time'+","
+                                  +'update_time'+","
+                                  +'end_time'+","
+                                  +'description'+","+"'name','attending_count','unsure_count','not_replied_count','all_members_count','timezone','ticket_uri','pic_small','pic','pic_big','pic_square','pic_cover','can_invite_friends','creator') VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
                                   
                                   
                         [fbStampToDbTime(eventAttrParse[i].start_time),
