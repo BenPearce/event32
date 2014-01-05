1a8c915a718b2c6d1eddc8cc5a3fff78bb431710
Your Name HereBen <benpearce1@gmail.com>
2014-01-04 17:43:51 -0800

    deleted some comments from fb_mobile.js

--- platforms/ios/www/js/fb_mobile.js
+++ platforms/ios/www/js/fb_mobile.js
@@ -16,35 +16,23 @@ var k;
 var lastEvIndex;
 
 function eventExpand(element){
-    /*
-     $("#event-wrap-in-"+cntIndex).height();
-     $("#event-wrap-in-"+cntIndex).css("height", "");
-     */
+
     var descPos;
-    //$("#event-wrap-in-"+dateId).css("height",$("#event-wrap-in-"+dateId).height() - $("#event-ex-"+lastEvIndex).height());
-    //var evId = $(element).data("id");
     var dateId = $(element).attr('data-dateHash');
     var evId = $(element).attr("id");
     evId = parseInt(evId);
-    
     //if(evId>lastEvIndex){
     descPos = $("#event-ex-"+evId).offset().top;
     //}
-    
     $("#event-ex-"+lastEvIndex).addClass("notransition");
     $("#event-ex-"+lastEvIndex).html("");
     $("#event-ex-"+lastEvIndex).css("height","");
     $("#event-ex-"+lastEvIndex).height();
     $("#event-ex-"+lastEvIndex).removeClass("notransition");
-    //$(document).scrollTop($(document).scrollTop() + $("#event-ex-"+evId).offset().top - descPos);//
-    //console.log("description: "+eventList[evId].description);
-    console.log("hash: "+dateId);
-    
     $("#event-wrap-in-"+dateId).addClass("notransition");
     $("#event-wrap-in-"+dateId).css("height","");
     $("#event-wrap-in-"+dateId).height();
     $("#event-wrap-in-"+dateId).removeClass("notransition");
-    
     $("#bot-elem").html(eventList[evId].description);
     
     var txHeight = $("#bot-elem").height();
@@ -52,42 +40,23 @@ function eventExpand(element){
     $("#bot-elem").html("");
     
     var description = eventList[evId].description;
-    //$("#event-ex-"+evId).html(generateButtons(evId) + description);
-    
-    
+
     $("#event-ex-"+evId).html(generateButtons(evId) +getDescriptionHtml(description));
-    //$("#event-ex-"+evId).html(description);
-    //$("#event-ex-"+evId).css(txHeight);
-    //$("#event-ex-"+evId).css("display","block");
     $("#event-ex-"+evId).css("height",txHeight + 50);
     $("#event-wrap-in-"+dateId).height();
-    //$("#event-wrap-in-"+dateId).css("height",$("#event-wrap-in-"+dateId).height() + $("#event-ex-"+evId).height());
     lastEvIndex = evId;
     // if(evId>lastEvIndex){
     $(document).scrollTop($(document).scrollTop() + $("#event-ex-"+evId).offset().top - descPos);
     setButtons();
     //}
-    /*
-     //k = $(elem).attr('data-dateId');
-     */
-    //$("#event-wrap-in-"+dateId).
-    
-    //setSwipe($("#event-ex-"+evId));
-    
 }
 
 function contract(shortRow,exIndex,elem,transition){
-    console.log("contract");
     $(elem).removeClass("expanded");
     if(shortRow){
-        console.log("short row triggered");
         if(transition){
-            console.log("short cnt row transition block");
             $("#event-wrap-in-"+exIndex).css("height","0px");
-            //$("#event-wrap-in-"+exIndex).html("");
-            //$doc.scrollTop($doc.scrollTop() + $("#date-elem-"+k+"-list").offset().top - pos);
         }else{
-            console.log("short cnt row non-transition block");
             $("#event-wrap-in-"+exIndex).addClass("notransition");
             $("#event-wrap-in-"+exIndex).css("height","0px");
             $("#event-wrap-in-"+exIndex).html("");
@@ -98,13 +67,11 @@ function contract(shortRow,exIndex,elem,transition){
         
     }else{
         if(transition){
-            console.log("long row cnt transition block");
             $("#event-wrap-in-"+exIndex).css("height","222px");
             if(exIndex != lastIndex){
                 $("#event-wrap-in-"+exIndex).html(getEventRow(eventList[dateHash[exIndex].eventList[0]].fbId,"",exIndex)+getEventRow(eventList[dateHash[exIndex].eventList[1]].fbId,"",exIndex)+getEventRow(eventList[dateHash[exIndex].eventList[2]].fbId,"",exIndex));
             }
-            
-            //$doc.scrollTop($doc.scrollTop() + $("#date-elem-"+k+"-list").offset().top - pos);
+
         }else{
             console.log("long row cnt non-transition block");
             $("#event-wrap-in-"+exIndex).addClass("notransition");
@@ -118,9 +85,6 @@ function contract(shortRow,exIndex,elem,transition){
 }
 
 function expand(cntIndex,elem){
-    //var dfd = $.Deferred();
-    //$("#event-wrap-in-"+k).removeClass("animateHeight");
-    console.log("expand");
     var exHtml = "";
     for(i=0;i<=dateHash[cntIndex].eventList.length - 1;i++){
         var exHtml = exHtml +getEventRow(eventList[dateHash[k].eventList[i]].fbId,"",cntIndex);
@@ -131,26 +95,11 @@ function expand(cntIndex,elem){
     $("#event-wrap-in-"+cntIndex).html(exHtml);
     $("#event-wrap-in-"+cntIndex).css("height",exHeight);
     dateLength[cntIndex] = exHeight;
-    
-    
-    
-    /*
-     $("#event-wrap-in-"+cntIndex).one('crossBrowserEvent',function(e) {
-     alert("cross 2");
-     });
-     */
-    //dfd.resolve("tx1");
-    //return dfd.promise();
 }
 
 function popDate(e,elem){
-    console.log("pop date");
     k = $(elem).attr('data-dateId');
-    console.log("K: "+k);
-    console.log("html: "+$("date-elem-"+k+"-list").html());
     var $this = $("date-elem-"+k+"-list");
-    //var $this = $(this);
-    
     pos = $("#date-elem-"+k+"-list").offset().top;
     $doc = $(document);
     firstExpand = (lastIndex == null);
@@ -160,23 +109,19 @@ function popDate(e,elem){
     expanded = $(elem).is(".expanded");
     
     if(shortRow & expanded){
-        console.log("shortRow & expanded");
         if((lastIndex != k) & !firstExpand){
-            console.log("money spot");
-            
+
             if(formerShortRow){
                 contract(formerShortRow,lastIndex,exSelector,true);
             }else{
                 contract(formerShortRow,lastIndex,exSelector,false);
             }
-            console.log(" before primary short row contract");
             contract (shortRow,k,elem,true);
         }else{
             contract (shortRow,k,elem,true);
         }
         
     } else if (shortRow & !expanded){
-        console.log("shortRow & !expanded");
         if((lastIndex != k) & !firstExpand){
             if(formerShortRow){
                 contract(formerShortRow,lastIndex,exSelector,true);
@@ -189,10 +134,7 @@ function popDate(e,elem){
         }
         
     }else if(!shortRow & expanded){
-        console.log("!shortRow & expanded");
-        
         if((lastIndex != k) & !firstExpand){
-            console.log("money spot");
             contract(formerShortRow,lastIndex,exSelector,false);
             contract(shortRow,k,elem,true);
         }else{
@@ -200,9 +142,7 @@ function popDate(e,elem){
         }
         
     }else if(!shortRow & !expanded){
-        console.log("!shortRow & !expanded");
         if((lastIndex != k) & !firstExpand){
-            console.log("money spot");
             contract(formerShortRow,lastIndex,exSelector,false);
             expand(k,elem);
         }else{
@@ -225,18 +165,7 @@ function popCal(){
         if(typeof dateHash[i] != 'undefined'){
             
             $("#dateMainList").append("<li data-role='list-divider' class='date-list-elem-outter'  id='date-elem-"+i+"-list' name='"+i+"' >"+getDateHeader(dateHash[i],i)+"<div id='event-list-wrap-trans-"+i+"' class='event-list-wrap-trans'><div id='event-wrap-in-"+i+"' class='event-wrap-in animateHeight'></div></div></li>");
-            
-            //$("#dateDisplayer-"+i).append(getMorebutton(i,moreText,"expanded"));
-            /*
-             uiEventCount = uiEventCount +parseInt(dateHash[i].eventList.length);
-             $("#event-wrap-in-"+i).append(getEventRow(eventList[dateHash[i].eventList[0]].fbId,"topEvent"));
-             }else{
-             continue;
-             */
-            
 
-            
-            
             if(typeof dateHash[i].eventList[0] != "undefined"){
                 $("#event-wrap-in-"+i).append(getEventRow(eventList[dateHash[i].eventList[0]].fbId,"topEvent",i));
             }else{
@@ -276,10 +205,7 @@ function popCal(){
     
     stickyList();
     setFriendTap();
-    
     setEventTap();
-    
-    
 }
 
 document.addEventListener('deviceready', function () {
