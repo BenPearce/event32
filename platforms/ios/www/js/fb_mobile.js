0b56ee0372cdef9032d856872a96a73f2a2a1830
Your Name HereBen <benpearce1@gmail.com>
2014-01-07 14:40:50 -0800

    formatting midway point

--- platforms/ios/www/js/fb_mobile.js
+++ platforms/ios/www/js/fb_mobile.js
@@ -192,7 +192,7 @@ function popCal(){
                 dateHeader = getDateHeader(dateHash[i],i,"","ex");
             }
             
-            $("#dateMainList").append("<li data-role='list-divider' class='date-list-elem-outter'  id='date-elem-"+i+"-list' name='"+i+"' >"+dateHeader+"<div id='event-list-wrap-trans-"+i+"' class='event-list-wrap-trans'><div id='event-wrap-in-"+i+"' class='event-wrap-in animateHeight'></div></div></li>");
+            $("#dateMainList").append("<li data-role='list-divider' class='date-list-elem-outter'  id='date-elem-"+i+"-list' name='"+i+"' >"+dateHeader+"<div id='event-list-wrap-trans-"+i+"' class='event-list-wrap-trans'><div id='event-wrap-in-"+i+"' class='event-wrap-in animateHeight'><div class='spacer'></div></div></div></li>");
 
             if(typeof dateHash[i].eventList[0] != "undefined"){
                 $("#event-wrap-in-"+i).append(getEventRow(eventList[dateHash[i].eventList[0]].fbId,"topEvent",i));
