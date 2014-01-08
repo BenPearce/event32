b0418737e4e1efd8271e5d376f0acd6227221af3
Your Name HereBen <benpearce1@gmail.com>
2014-01-08 02:03:33 -0800

    changed approach first stage dynamics working

--- platforms/ios/www/js/imageCluster.js
+++ platforms/ios/www/js/imageCluster.js
@@ -38,7 +38,9 @@ function getEventRow(eventId,eventClass,datIndex){
         
                         //return '<div class="row-wrap '+eventClass+'">'+eventImg+''+friendImg+'<div class="event-wrap row-title-wrap event-title-wrap"><div class="event-name">'+eventName+'</div><div class="friend-name">'+friendName1+'</div></div></div><div id="event-ex-'+eventId+'" class="event-ex"></div>';
         
-          return '<div class="row-wrap '+eventClass+'"><div class="event-outter">'+eventImg+'<div class="event-name"><div class="event-name-inner">'+eventName+'</div></div><div class="event-exp"></div></div><div class="friend-outter">'+friendImg+'<div class="friend-name"><div class="friend-name-inner">'+friendName1+'</div></div><div class="friend-exp"></div></div></div><div id="event-ex-'+eventId+'" class="event-ex"></div>';
+          //return '<div class="row-wrap '+eventClass+'"><div class="event-outter">'+eventImg+'<div class="event-name"><div class="event-name-inner">'+eventName+'</div></div><div class="event-buttons"></div><div class="event-exp"></div></div><div class="friend-outter">'+friendImg+'<div class="friend-name"><div class="friend-name-inner">'+friendName1+'</div></div><div class="friend-buttons"></div><div class="friend-exp"></div></div></div><div id="event-ex-'+eventId+'" class="event-ex"></div>';
+        
+                  return '<div class="row-wrap '+eventClass+'"><div class="event-outter">'+eventImg+'<div class="event-name"><div class="event-name-inner">'+eventName+'</div></div></div><div class="friend-outter">'+friendImg+'<div class="friend-name"><div class="friend-name-inner">'+friendName1+'</div></div></div></div><div id="event-ex-'+eventId+'" class="event-ex"></div>';
         
     } else {
         var pre = '<img class="event-friend-img-quad" style="pointer-events:none" width=25 height=25 src="';
