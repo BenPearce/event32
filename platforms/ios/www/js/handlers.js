e40537b150545fa4264e55e69bcd2243c2071a9f
Your Name HereBen <benpearce1@gmail.com>
2014-05-09 16:09:14 -0700

    deleted comment

--- platforms/ios/www/js/handlers.js
+++ platforms/ios/www/js/handlers.js
@@ -506,17 +506,7 @@ function setEventTap() {
                                     $(this).parents().eq(4).addClass("notransition");
                                     var tempHeight = $(this).parents().eq(3).height();
                                     $(this).parents().eq(2).addClass('event-selected');
-                                    /*
-                                    if (formerEventSelected) {
-                                    
-                                    } else {
-   
-                                    if (!formerEventSelected) {
-                                    $(this).parents().eq(3).css('height', tempHeight + 65);
-                                    }
-                                    
-                                    }
-                                    */
+
                                     if (!formerEventSelected & !formerEventSelected) {
                                     $(this).parents().eq(3).css('height', tempHeight + 65);
 
